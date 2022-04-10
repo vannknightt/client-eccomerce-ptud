f7fa4841ab536cc1e5d4d1da19ae0ab6bb7d015e
vannknightt <nvhninojr@gmail.com>
2022-04-10 17:16:17 +0700

    add new avatar image

--- src/api-services/shipperService.js
+++ src/api-services/shipperService.js
@@ -1,6 +1,8 @@
 import Axios from 'axios';
 
 const RESOURCE_NAME = '/api/shipper';
+
+// const pathjava = 'http://localhost:8080'
 const pathjava = 'https://java-ecommerce-ptud.herokuapp.com'
 
 export default {
